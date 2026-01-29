0368102533dd2d105245ce9d45d2a4e5957488e2
sweety62 <myselfsweety1@gmail.com>
2026-01-29 22:38:04 +0530

    Add initial JavaScript and HTML files with basic console logging

---
+++ first.js
@@ -0,0 +1,18 @@
+
+console.log("welcome to apna college!");
+const student = {
+    fullName:"sweety",
+    age:20,
+    cgpa:8.2,
+    ispass:true
+
+
+};
+console.log(student);
+const product ={
+    title:"Ball pen",
+    rating:4,
+    offer:5,
+    price:270,
+};
+console.log(product);
