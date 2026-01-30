d803100ca862d6d5b9e06a1f532ad5e464d11b9b
sweety62 <myselfsweety1@gmail.com>
2026-01-30 15:39:13 +0530

    Add initial HTML and JavaScript files with basic structure and console logging

---
+++ script.js
@@ -0,0 +1,44 @@
+//this code prints hello world
+//console.log("Hello, World!");
+
+//arithmetic operations
+/*let a = 5;
+let b = 2;
+console .log("a=",a , " &b=",b);
+console.log("a+b",a+b);
+console.log("a-b",a-b);
+console.log("a*b",a*b);
+console.log("a/b",a/b);
+console.log("a%b",a%b);
+console.log("a**b",a**b);*/
+
+//unary operators
+let a = 5;
+let b = 2;
+console .log("a=",a , " &b=",b);
+console.log("++a=",++a);
+console.log("--b=",--b);
+console.log("a++=",a++);
+console.log("b--=",b--);
+
+//assignment operators
+let c = 5;
+let d = 2;
+console.log("c=",c , " &d=",d);
+console.log("c+=d",c+=d);
+console.log("c-=d",c-=d);
+console.log("c*=d",c*=d);
+console.log("c/=d",c/=d);
+console.log("c%=d",c%=d);
+console.log("c**=d",c**=d);
+
+//comparison operators
+console.log("a==b",a==b);
+console.log("a===b",a===b);
+console.log("a!=b",a!=b);
+console.log("a!==b",a!==b);
+console.log("a>b",a>b);
+console.log("a<b",a<b);
+console.log("a>=b",a>=b);
+console.log("a<=b",a<=b);
+
