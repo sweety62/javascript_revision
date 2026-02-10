4af0acb525179775337e37f4f7582f6d22c15505
sweety62 <myselfsweety1@gmail.com>
2026-02-10 19:27:00 +0530

    Refactor ToyotaCar class to use constructor for brand initialization

--- class.js
+++ class.js
@@ -1,6 +1,6 @@
 //creating classes
 
-class ToyotaCar{
+/*class ToyotaCar{
     start(){
         console.log("start");
     }
@@ -18,3 +18,25 @@ let fortuner = new ToyotaCar();
 fortuner.setBrand("Fortuner");
 let lexus = new ToyotaCar();
 lexus.setBrand("Lexus");
+*/
+
+class ToyotaCar {
+    constructor(brand) {
+        this.brandName = brand;
+    }
+
+    start() {
+        console.log("start");
+    }
+
+    stop() {
+        console.log("stop");
+    }
+
+    setBrand(brand) {
+        this.brandName = brand;
+    }
+}
+
+let fortuner = new ToyotaCar("Fortuner");
+let lexus = new ToyotaCar("Lexus");
