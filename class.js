f5b401b896654bef26829158918c4380266660f8
sweety62 <myselfsweety1@gmail.com>
2026-02-10 19:45:30 +0530

    Add inheritance and method overriding examples with Parent and Engineer classes

--- class.js
+++ class.js
@@ -40,3 +40,49 @@ class ToyotaCar {
 
 let fortuner = new ToyotaCar("Fortuner");
 let lexus = new ToyotaCar("Lexus");
+
+
+
+
+//inheritance
+class Parent {
+    hello() {
+        console.log("Hello from Parent");
+    }
+}
+
+class Child extends Parent {
+    hello() {
+        console.log("Hello from Child");
+    }
+}
+
+let child = new Child();
+child.hello();  // Output: Hello from Child
+
+
+
+
+
+//method overriding
+
+class person{
+    eat(){
+        console.log("person is eating");
+        
+    }
+    sleep(){
+        console.log("person is sleeping");
+    }
+    work(){
+        console.log("do nothing");
+    }
+}
+
+class Engineer extends person {
+    work(){
+        console.log("Engineer is working");
+    }
+}
+
+let eng = new Engineer();
